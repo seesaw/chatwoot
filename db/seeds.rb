83a98fcbe1809e3d1b83210fac19bf9d78708407
Nicola <pagiaro@gmail.com>
2020-01-31 17:28:41 +0100

    setup changed setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-account = Account.create!(name: 'Acme Inc')
+account = Account.create!(name: 'Seesaw srl')
 
-user = User.new(name: 'John', email: 'john@acme.inc', password: '123456', account: account, role: :administrator)
+user = User.new(name: 'John', email: 'john@acme.inc', password: 'password', account: account, role: :administrator)
 user.skip_confirmation!
 user.save!
 
-web_widget = Channel::WebWidget.create!(account: account, website_name: 'Acme', website_url: 'https://acme.inc')
+# web_widget = Channel::WebWidget.create!(account: account, website_name: 'Acme', website_url: 'https://acme.inc')
 
-inbox = Inbox.create!(channel: web_widget, account: account, name: 'Acme Support')
-InboxMember.create!(user: user, inbox: inbox)
+# inbox = Inbox.create!(channel: web_widget, account: account, name: 'Acme Support')
+# InboxMember.create!(user: user, inbox: inbox)
 
-contact = Contact.create!(name: 'jane', email: 'jane@example.com', phone_number: '0000', account: account)
-contact_inbox = ContactInbox.create!(inbox: inbox, contact: contact, source_id: user.id)
-conversation = Conversation.create!(account: account, inbox: inbox, status: :open, assignee: user, contact: contact, contact_inbox: contact_inbox)
-Message.create!(content: 'Hello', account: account, inbox: inbox, conversation: conversation, message_type: :incoming)
+# contact = Contact.create!(name: 'jane', email: 'jane@example.com', phone_number: '0000', account: account)
+# contact_inbox = ContactInbox.create!(inbox: inbox, contact: contact, source_id: user.id)
+# conversation = Conversation.create!(account: account, inbox: inbox, status: :open, assignee: user, contact: contact, contact_inbox: contact_inbox)
+# Message.create!(content: 'Hello', account: account, inbox: inbox, conversation: conversation, message_type: :incoming)
